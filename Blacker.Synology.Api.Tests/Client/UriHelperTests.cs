05119dbfdff3c5a0c471861668019fb1ff9e7599
Lukáš Černý <cerny.l@gmail.com>
2017-01-07 17:16:34 +0100

    Reworked UriHelper to handle escaping of values correctly

--- Blacker.Synology.Api.Tests/Client/UriHelperTests.cs
+++ Blacker.Synology.Api.Tests/Client/UriHelperTests.cs
@@ -12,16 +12,16 @@ namespace Blacker.Synology.Api.Tests.Client
         public void BuildUriThrowsOnNullArgumentTest()
         {
             Assert.DoesNotThrow(() => UriHelper.BuildUri(new Uri("http://localhost/"), "path", new Dictionary<string, object>()));
+            Assert.DoesNotThrow(() => UriHelper.BuildUri(new Uri("http://localhost/"), "path", null));
 
             Assert.Throws<ArgumentNullException>(() => UriHelper.BuildUri(null, "path", new Dictionary<string, object>()));
             Assert.Throws<ArgumentNullException>(() => UriHelper.BuildUri(new Uri("http://localhost/"), null, new Dictionary<string, object>()));
-            Assert.Throws<ArgumentNullException>(() => UriHelper.BuildUri(new Uri("http://localhost/"), "path", null));
         }
 
         [TestCaseSource(nameof(BuildUriCreatesValidUriTestCaseData))]
         public void BuildUriCreatesValidUriTest(Uri baseUri, string path, IDictionary<string, object> parameters, string expected)
         {
-            Assert.That(UriHelper.BuildUri(baseUri, path, parameters).ToString(), Is.EqualTo(expected));
+            Assert.That(UriHelper.BuildUri(baseUri, path, parameters).AbsoluteUri, Is.EqualTo(expected));
         }
 
         static IEnumerable<TestCaseData> BuildUriCreatesValidUriTestCaseData
@@ -33,7 +33,7 @@ namespace Blacker.Synology.Api.Tests.Client
                 yield return new TestCaseData(new Uri("http://localhost/base"), "/path", new Dictionary<string, object>(), "http://localhost/path");
                 yield return new TestCaseData(new Uri("http://localhost/"), "path", new Dictionary<string, object>() {{"query", "value"}}, "http://localhost/path?query=value");
                 yield return new TestCaseData(new Uri("http://localhost/"), "path", new Dictionary<string, object>() {{"query", 1}}, "http://localhost/path?query=1");
-                yield return new TestCaseData(new Uri("http://localhost/"), "path", new Dictionary<string, object>() {{"query", "val&value"}}, "http://localhost/path?query=val%2526value");
+                yield return new TestCaseData(new Uri("http://localhost/"), "path", new Dictionary<string, object>() {{"query", "val&value"}}, "http://localhost/path?query=val%26value");
                 yield return new TestCaseData(new Uri("http://localhost/"), "path", new Dictionary<string, object>() {{"query", "value"}, {"query2", "value2"} }, "http://localhost/path?query=value&query2=value2");
             }
         }
--- Blacker.Synology.Api/Client/UriHelper.cs
+++ Blacker.Synology.Api/Client/UriHelper.cs
@@ -7,26 +7,26 @@ namespace Blacker.Synology.Api.Client
 {
     class UriHelper
     {
-        public static Uri BuildUri(Uri baseUri, string path, IDictionary<string, object> parameters)
+        public static Uri BuildUri(Uri baseUri, string path, IDictionary<string, object> parameters = null)
         {
             if (baseUri == null)
                 throw new ArgumentNullException(nameof(baseUri));
             if (path == null)
                 throw new ArgumentNullException(nameof(path));
-            if (parameters == null)
-                throw new ArgumentNullException(nameof(parameters));
 
-            var queryParams = HttpUtility.ParseQueryString("");
+            var uriBuilder = new UriBuilder(baseUri);
 
-            foreach (var parameter in parameters)
+            if (parameters != null)
             {
-                queryParams[parameter.Key] = HttpUtility.UrlEncode(parameter.Value?.ToString() ?? String.Empty);
-            }
+                var queryParams = HttpUtility.ParseQueryString("");
 
-            var uriBuilder = new UriBuilder(baseUri)
-                             {
-                                 Query = queryParams.ToString()
-                             };
+                foreach (var parameter in parameters)
+                {
+                    queryParams.Add(parameter.Key, parameter.Value?.ToString() ?? String.Empty);
+                }
+
+                uriBuilder.Query = queryParams.ToString();
+            }
 
             uriBuilder.Path = Path.Combine(uriBuilder.Path, path).Replace('\\', '/');
 
